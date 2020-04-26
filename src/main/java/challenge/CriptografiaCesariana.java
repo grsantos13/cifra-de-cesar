ed3835534cf7e597082176d12ca4d507a5def1c8
grsantos13 <grodrigueset@gmail.com>
2020-04-26 16:45:33 -0300

    Alteração de variável casas para constante CASAS e adicionados algumas linhas para melhor legibilidade.

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -1,38 +1,39 @@
 package challenge;
 
 public class CriptografiaCesariana implements Criptografia {
-    byte casas = 3;
-    @Override
-    public String criptografar(String texto) {
-        System.out.println(gerarTextoProcessado(texto, "criptografar", casas));
-        return gerarTextoProcessado(texto, "criptografar", casas);
-    }
+    final byte CASAS = 3;
 
     @Override
+    public String criptografar(String texto) {
+        return gerarTextoProcessado(texto, "criptografar", CASAS);
+    }
+    @Override
     public String descriptografar(String texto) {
-        return gerarTextoProcessado(texto, "decriptografar", casas);
+        return gerarTextoProcessado(texto, "decriptografar", CASAS);
     }
     private String gerarTextoProcessado(String texto, String operacao, int casas){
         if (texto.isEmpty()){
             throw new IllegalArgumentException("Texto em branco não são permitidos");
         }
+
         String finalText = new String();
         int letra;
         texto = texto.toLowerCase();
+
         for (int i = 0; i < texto.length(); i++){
             if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z'){
-                letra = (operacao == "criptografar") ? texto.charAt(i) + casas : texto.charAt(i) - casas;
+                letra = (operacao.equals("criptografar")) ? texto.charAt(i) + casas : texto.charAt(i) - casas;
                 if ((char) letra <'a'){
                     letra = letra + 26 + casas;
                 }else if((char) letra > 'z'){
                     letra = letra - 26 - casas;
                 }
-
             }else{
                 letra = texto.charAt(i);
             }
             finalText = finalText.concat(String.valueOf((char) letra));
         }
+
         return finalText;
     }
 }
